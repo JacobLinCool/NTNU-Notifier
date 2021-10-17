7fe3e485d2c8f443715968a03a0ee25a8022e27c
JacobLinCool <jacoblincool@gmail.com>
2021-10-17 16:11:18 +0800

    fix(covid19): fix news selector

--- src/notifiers/covid19.ts
+++ src/notifiers/covid19.ts
@@ -11,7 +11,6 @@ async function checkout() {
     const news: News[] = [];
     const links: string[] = [];
     $("table tr > td:first-child > a").each((idx, elm) => {
-        if (idx === 0 || idx >= 16) return;
         const link = $(elm).attr("href");
         if (link) links.push(link);
     });
@@ -23,34 +22,31 @@ async function checkout() {
             const title = $("table tr:nth-child(1) > td").text().trim();
             const date = new Date($("table tr:nth-child(2) > td").text());
             const type = $("table tr:nth-child(3) > td").text().trim().split(" ");
-            news.push({
-                title,
-                url,
-                date,
-                type,
-            });
+            news.push({ title, url, date, type });
         }),
     );
 
-    return news;
+    return news.sort((a, b) => b.date.getTime() - a.date.getTime());
 }
 
+async function check(notifier: Covid19Notifier) {
+    const news = await checkout();
+    const newNews = news.filter((x) => !notifier.memory.find((y) => y.url === x.url));
+    for (let i = newNews.length - 1; i >= 0; i--) {
+        notifier.memory.push(newNews[i]);
+        notifier.notify(newNews[i]);
+    }
+    while (notifier.memory.length > 15) notifier.memory.shift();
+}
+
+/** Covid19Notifier 的資料來源是 https://covid19.ntnu.edu.tw/news_listV2.php */
 class Covid19Notifier extends Notifier {
+    public memory: News[] = [];
+
     constructor() {
         super();
         this.name = "Covid19-Notifier";
-        this.on("init", () => {});
-        this.on("check", this.check);
-    }
-
-    async check() {
-        const news = await checkout();
-        const newNews = news.filter((x) => !this.memory.find((y) => y.url === x.url));
-        for (let i = newNews.length - 1; i >= 0; i--) {
-            this.memory.push(newNews[i]);
-            this._notify(this, newNews[i]);
-        }
-        while (this.memory.length > 15) this.memory.shift();
+        this.on("check", () => check(this));
     }
 
     recall(news: News[]): Notifier {
